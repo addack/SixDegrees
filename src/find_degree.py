b56b7eb78d1e7811e47457899852d174bc4a2d6d
addack <michel.tricot@gmail.com>
2011-05-01 19:13:42 +0200

    add dedicated class for computation

--- src/find_degree.py
+++ src/find_degree.py
@@ -9,35 +9,19 @@ import logging
 import sys
 
 import graph
-import layer_generator
-
-def find_degree(g, from_actor, to_actor):
-    layers = layer_generator.LayerGenerator(g, from_actor)
-    degree = 0
-    for layer in layers:
-        if to_actor in layer:
-            break
-        degree += 1
-    return degree
-
-def find_max_degree(g, from_actor):
-    layers = layer_generator.LayerGenerator(g, from_actor)
-    old_layer = None
-    for layer in layers:
-        old_layer = layer
-    print old_layer
+import degree_finder
 
 def main():
-    logging.basicConfig(level=logging.INFO)
     g = graph.load_graph(sys.argv[1])
     s = g.getInfo()
     logging.info('#actors: %s, #movies: %s' % (s.actors_count, s.movies_count))
-    
-    #print find_degree(g, 'Pitt, Brad', 'Reno, Jean (I)')
-    print find_max_degree(g, 'Pitt, Brad')
-    #print find_degree(g, 'Pitt, Brad', 'Hong, Duyen')
-    #print find_degree(g, 'Pitt, Brad', 'Klooren, Mati')
-    
+
+    degree = degree_finder.DegreeFinder(g)    
+    print degree.computeMaxDegree('Pitt, Brad')
+    print degree.computeDegree('Pitt, Brad', 'Hong, Duyen')
+    print degree.computeDegree('Pitt, Brad', 'Klooren, Mati')
+    print degree.computeDegree('Pitt, Brad', 'Reno, Jean (I)')
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     main()
